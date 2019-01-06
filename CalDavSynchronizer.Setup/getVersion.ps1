5fe14c2e5b068884875ead35fdc5efc6b253172d
Casey Holzer <caseyh@snapappointments.com>
2019-01-06 11:50:20 -0700

    pass through config (debug or release) to getVersion script

--- CalDavSynchronizer.Setup/getVersion.ps1
+++ CalDavSynchronizer.Setup/getVersion.ps1
@@ -1,7 +1,8 @@
 $1=$args[0]
+$3=$args[1]
 
 $parent = (Get-Item $1).parent.FullName
 
-$version = (Get-Item $parent"\CalDavSynchronizer\obj\Debug\CalDavSynchronizer.dll").VersionInfo.ProductVersion
+$version = (Get-Item $parent"\CalDavSynchronizer\obj\$3\CalDavSynchronizer.dll").VersionInfo.ProductVersion
 
 Out-File -FilePath $1"version.txt" -InputObject $version -Encoding ASCII -Width 50
--- CalDavSynchronizer.Setup/postbuild.bat
+++ CalDavSynchronizer.Setup/postbuild.bat
@@ -1,6 +1,6 @@
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x86\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /sha1 "86B0AE3B15989A793D3FAAC6187A8136F45EB52E" %2
 
-powershell.exe -ExecutionPolicy ByPass -File "%1getVersion.ps1" %1
+powershell.exe -ExecutionPolicy ByPass -File "%1getVersion.ps1" %1 %3
 set /p version=<%1version.txt
 "C:\Program Files (x86)\NSIS\makensis.exe" /DVERSION=%version% /DCONFIG=%3 %1SnapAppointmentsOutlookSync.nsi
 del %1version.txt
